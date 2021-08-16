12c7ad3324e1c29ad27abc99c82c956db47d6e77
LalwaniVritti <88480121+LalwaniVritti@users.noreply.github.com>
2021-08-16 16:29:14 +0530

    Add files via upload
    new platforms added

--- sketch.js
+++ sketch.js
@@ -22,8 +22,7 @@ var gameOver, restart;
 function preload(){
   
   playerImage=loadImage("images/jack2.png");
-  pImage = loadImage("images/p1.png");
-  bgImage = loadImage("images/download.jpeg");
+  bgImage = loadImage("images/bg.png");
   
   breakImage = loadImage("images/brokenPlatform.png");
   platform1 = loadImage("images/platform1.png");
@@ -46,18 +45,16 @@ function setup() {
   
   bg = createSprite(250,300);
   bg.addImage(bgImage);
-  bg.scale=4;
+
 
   player = createSprite(250,300,20,50);
-  
   player.addImage(playerImage);
   player.scale = 1;
   player.depth=10000;
   
   p = createSprite(250,350);
-  p.addImage(pImage);
-  p.scale=0.15
-  
+  p.addImage(platform3);
+  p.scale=0.1;
   
   gameOver = createSprite(250,250);
   gameOver.addImage(gameOverImg);
@@ -68,8 +65,7 @@ function setup() {
 
   gameOver.visible = false;
   restart.visible = false;
-  invisibleGround = createSprite(250,600,500,10);
-  invisibleGround.visible = false;
+  
   platformGroup = new Group();
   newPlatformGroup = new Group();
   
@@ -94,12 +90,7 @@ function draw() {
   
   if (gameState===PLAY){
     spawnplatforms();
-    player.collide(p)
-
-    bg.velocityY = 5;
-    if(bg.y>450){
-      bg.y=bg.height/2;
-    }
+    //player.collide(p)
 
     // to restart the game frameCount can not be reset, so using frameRate
     score = score + Math.round(getFrameRate()/60)
@@ -123,7 +114,7 @@ function draw() {
     newHurdle();
     //add gravity
     player.velocityY = player.velocityY + 0.8
-    player.collide(invisibleGround);
+    //player.collide(invisibleGround);
     
 
     for(var i =0;i<newPlatformGroup.length;i++){
@@ -142,7 +133,7 @@ function draw() {
   else if (gameState === END) {
     gameOver.visible = true;
     restart.visible = true;
-    bg.velocityY=0;
+    
     //set velcity of each game object to 0  
     player.velocityY = 2;
     platformGroup.setVelocityXEach(0);
@@ -158,7 +149,7 @@ function draw() {
   
   drawSprites();
   textSize(20);
-  fill(0);
+  fill(255);
   text(" Score  :  " + score, 350,50);
   
 }
@@ -167,12 +158,13 @@ function reset(){
   player.x = 250;
   player.y=300;
   p.y=350;
+  player.velocityY = 0;
   gameState = START;
   platformGroup.destroyEach();
   score = 0;
   restart.visible = false;
   gameOver.visible= false;
-  player.velocityY = -5;
+  
 }
 
 
@@ -181,8 +173,9 @@ function spawnplatforms() {
 
   if(frameCount % 40 == 0) {
     xx=Math.round(random(50,450))
-    var platform = createSprite(xx,-10,150,20);
-    //platform.setCollider("rectangle",0,0,50,14);
+    var platform = createSprite(xx,-10,150,30);
+    
+    platform.setCollider("rectangle",0,0,300,30);
     platform.velocityY = 4;
 
     var r = Math.round(random(1,5))
@@ -202,8 +195,6 @@ function spawnplatforms() {
  
     //assign scale and lifetime to the platform           
     platform.scale = 0.1;
-    //platform.setCollider("rectangle",30,-100,600,20);
-
     platform.lifetime = 200;
     
     //add each platform to the group
@@ -217,24 +208,17 @@ function newHurdle(){
   if(frameCount % 100 == 0) {
     xx=Math.round(random(50,450))
     var plat = createSprite(xx,-10,150,20);
-    //plat.setCollider("rectangle",0,0,50,14);
     plat.velocityY = 4;
-    //platform.debug=true
-    //plat.depth=0
-    plat.addImage(platform6)
- 
-    //assign scale and lifetime to the platform           
+    
+    plat.addImage(platform6);         
     plat.scale = 0.1;
-    //platform.setCollider("rectangle",30,-100,600,20);
+    
 
     plat.lifetime = 200;
     plat.collide(player);
     
     //add each platform to the group
     newPlatformGroup.add(plat);
-   
-   
-  
 }
 
 }
